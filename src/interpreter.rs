f708ca3f11b215cbcc1d241e2ee89dc7782f096a
Dominic Valerio <devalerio@outlook.com>
2022-07-06 15:03:19 -0400

    m

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -24,19 +24,20 @@ impl Interpreter {
     }
   }
 
-  pub fn run(&mut self) {
+  pub fn run(&mut self) -> Result<(), String> {
     while let Some(statement) = self.ast.next() {
-      self.run_statement(&statement.clone());
+      self.run_statement(statement.clone())?;
     }
+    Ok(())
   }
 
-  pub fn run_statement(&mut self, statement: &Statement) -> Option<ReturnVal> {
+  pub fn run_statement(&mut self, statement: Statement) -> Result<Option<ReturnVal>, String> {
     match statement {
       Statement::Expression(expr) => {
-        self.run_expression(expr);
+        self.run_expression(expr)?;
       }
       Statement::VarDeclaration{name, value} => {
-        self.run_var(name, value); 
+        self.run_var(&name, value)?; 
       }
       Statement::FunctionDeclaration{name, params, body} => {
         self.globals.insert(name.clone(), Value::Function{
@@ -45,56 +46,54 @@ impl Interpreter {
       }
       Statement::While{condition, body} => {
         loop {
-          if let Value::Bool(cond) = self.run_expression(condition) {
+          if let Value::Bool(cond) = self.run_expression(condition.clone())? {
             if cond == false { 
               break; 
             }
 
-            for statement in body {
-              self.run_statement(statement);
+            for statement in body.iter() {
+              self.run_statement(statement.clone())?;
             }
           }
         }
       }
       Statement::Return(expr) => {
-        return Some(self.run_expression(expr));
+        return Ok(Some(self.run_expression(expr)?));
       }
     }
-    Option::None
+    Ok(Option::None)
   }
 
-  pub fn run_expression(&mut self, expression: &Expression) -> Value {
+  pub fn run_expression(&mut self, expression: Expression) -> Result<Value, String> {
     use Value::*;
     use TokenKind::*;
 
-    match expression {
+    let res = match expression {
       // Literals
-      Expression::Int(v) => Int(*v),
+      Expression::Int(v) => Int(v),
       Expression::Str(v) => Str(v.clone()),
-      Expression::Bool(v) => Bool(*v),
+      Expression::Bool(v) => Bool(v),
       // Binary Operation
       Expression::BinOp(left, op, right) => {
-        let left = self.run_expression(&**left);
-        let right = self.run_expression(&**right);
+        let left = self.run_expression(*left);
+        let right = self.run_expression(*right);
 
-        match (left.clone(), op, right.clone()) {
-          (Int(l), Plus, Int(r)) => return  Int(l + r),
-          (Int(l), Mul, Int(r)) => return  Int(l * r),
-          (Int(l), Slash, Int(r)) => return  Int(l / r),
-          (Int(l), Minus, Int(r)) => return Int(l - r),
-          (Int(l), Equals, Int(r)) => return Bool(l == r),
-          (Int(l), NotEquals, Int(r)) => return Bool(l != r),
-          (Int(l), LessThan, Int(r)) => return Bool(l < r),
-          (Int(l), GreaterThan, Int(r)) => return Bool(l > r),
-          (Int(l), LessEquals, Int(r)) => return Bool(l <= r),
-          (Int(l), GreaterEquals, Int(r)) => return Bool(l >= r),
+        match (left.clone()?, &op, right.clone()?) {
+          (Int(l), Plus, Int(r)) => Int(l + r),
+          (Int(l), Mul, Int(r)) =>  Int(l * r),
+          (Int(l), Slash, Int(r)) =>   Int(l / r),
+          (Int(l), Minus, Int(r)) =>  Int(l - r),
+          (Int(l), Equals, Int(r)) =>  Bool(l == r),
+          (Int(l), NotEquals, Int(r)) =>  Bool(l != r),
+          (Int(l), LessThan, Int(r)) =>  Bool(l < r),
+          (Int(l), GreaterThan, Int(r)) =>  Bool(l > r),
+          (Int(l), LessEquals, Int(r)) =>  Bool(l <= r),
+          (Int(l), GreaterEquals, Int(r)) =>  Bool(l >= r),
 
-          (Bool(l), Equals, Bool(r)) => return Bool(l == r),
-          (Bool(l), NotEquals, Bool(r)) => return Bool(l != r),
+          (Bool(l), Equals, Bool(r)) =>  Bool(l == r),
+          (Bool(l), NotEquals, Bool(r)) =>  Bool(l != r),
           _=> {
-            eprintln!("Invalid/unsupported operation.");
-            dbg!(left, op, right);
-            unimplemented!();
+            return Err(format!("Unsupported operation {:?} between {:?} and {:?}", op, left, right));
           }
         }
       }
@@ -102,15 +101,15 @@ impl Interpreter {
         let mut processed_args: Vec<Value> = Vec::new();
 
         for arg in args.into_iter() {
-            processed_args.push(self.run_expression(arg));
+            processed_args.push(self.run_expression(arg)?);
         }
 
-        return self.run_function(name, processed_args);
+        return self.run_function(&name, processed_args);
       }
       Expression::Identifier(name) => {
         // TODO: wtf does this mean?
-        if let Some(val) = self.globals.get(name) {
-          return val.clone();
+        if let Some(val) = self.globals.get(&name) {
+          val.clone()
         } else {
           unreachable!();
         }
@@ -119,23 +118,23 @@ impl Interpreter {
         Value::None
       }
       Expression::Assign(name, value) => {
-        self.run_assign(name, value);
+        self.run_assign(*name, *value)?;
         Value::None
       }
-    }
+    };
+    return Ok(res);
   }
 
-  fn run_function(&mut self, name: &String, args: Vec<Value>) -> Value {
+  fn run_function(&mut self, name: &String, args: Vec<Value>) -> Result<Value, String> {
     if let Some(v) = self.globals.clone().get(name) {
       match v {
         Value::NativeFunction{callback, ..} => {
           let retval = callback(args, self);
-          return retval;
+          return Ok(retval);
         },
         Value::Function{params, body, ..} => {
           if params.len() != args.len() {
-            eprintln!("Arguments of length {} don't match paramters of length {}", args.len(), params.len());
-            unimplemented!();
+            return Err(format!("Arguments of length {} don't match paramters of length {}", args.len(), params.len()));
           }
 
           for i in 0..params.len() {
@@ -143,42 +142,39 @@ impl Interpreter {
           }
 
           //dbg!(&self.globals)
-          //let mut return_val: Option<Value> = None;
 
-          for statement in body {
-            let res = self.run_statement(statement);
-
-            if let Some(ret_val) = res {
-              return ret_val;
+          for statement in body.iter() {
+            if let Some(retval) = self.run_statement(statement.clone())? {
+              return Ok(retval);
             }
           }
 
-          return Value::None;
+          return Ok(Value::None);
         }
         _=> unreachable!(),
       }
     } else {
-      unimplemented!(); // function doesn't exist
+      unreachable!(); // function doesn't exist
     }
   }
 
-  fn run_var(&mut self, name: &String, value: &Expression) {
-    let res = self.run_expression(value);
-    self.globals.insert(name.clone(), res);
+  fn run_var(&mut self, name: &String, value: Expression) -> Result<(), String> {
+    let right = self.run_expression(value)?;
+    self.globals.insert(name.clone(), right);
+    Ok(())
   }
 
-  fn run_assign(&mut self, name: &Expression, value: &Expression) -> Value {
-    let new_value = self.run_expression(value);
+  fn run_assign(&mut self, name: Expression, value: Expression) -> Result<Value, String> {
+    let new_value = self.run_expression(value)?;
 
-    if let Expression::Identifier(name) = name {
+    if let Expression::Identifier(name) = &name {
       if let Some(_old_value) = self.globals.get(name) {
         self.globals.insert(name.clone(), new_value);
-        return Value::None;
+        return Ok(Value::None);
       } 
     }
 
-    eprintln!("{:?} doesn't exist", name);
-    unimplemented!();
+    Err(format!("{:?} doesn't exist", name))
   }
 }
 

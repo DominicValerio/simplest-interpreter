df2c7f8e30a5c6d44b58ac2621a4427441116572
Dominic Valerio <devalerio@outlook.com>
2022-07-06 20:49:09 -0400

    m

--- src/tests/interpreter_test.rs
+++ src/tests/interpreter_test.rs
@@ -1,6 +1,6 @@
-use std::time::Instant;
+use std::{time::Instant, mem, cell::RefMut};
 
-use crate::{interpreter::*, lexer::*, parser::*};
+use crate::{interpreter::*, lexer::*, parser::*, environment::Value};
 
 #[test]
 fn simple() {
@@ -18,4 +18,39 @@ fn simple() {
     let instant = Instant::now();
     i.run().unwrap();
     println!("{}s", instant.elapsed().as_secs_f64());
+}
+
+struct Function {
+  name: String,
+  params: Vec<String>,
+  body: Vec<String>,
+}
+
+struct NativeFunction {
+  name: String,
+  callback: fn(i32) -> i32,
+}
+enum Test {
+  Nil,
+  Number(f64),
+  Str(String),
+  Bool(bool),
+  Function(Function),
+  NativeFunction(Box<NativeFunction>),
+}
+
+#[test]
+fn mem() {
+  use mem::*;
+
+  let x = &999999999;
+
+  dbg!(mem::size_of_val(&*x));
+
+  dbg!(mem::align_of::<Test>());
+  dbg!(mem::size_of::<Test>()); 
+  let inner = Function {name: "sdf".to_string(), params: vec![], body: vec![]};
+  dbg!(mem::size_of_val(&inner));
+  let x  = Test::Function(inner);
+  dbg!(mem::size_of_val(&x));
 }
